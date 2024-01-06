d8aa0c963fae123535332fcd511d9358b642eead
Arpit Choudhary <89199400+arpitchoudhary0206@users.noreply.github.com>
2024-01-06 11:49:37 +0000

    added data types notes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState
+
+console.log("accountId")
+console.table([accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"    // Code will be treated as newer vesion of JS
+
+alert(3+3)      // It will not work in node js just like browser
+
+// ECMA script is an organization that defines Coding standards
+
+let name = "Arpit"
+let age = 28
+let isAlive = true
+
+// Data types
+/*  Primitive Data types
+number range 2 to the power 53
+bigint = large numbers
+string = ""
+boolean = true/false
+null => standalone value
+    let's say asking server to give temperature, now if it is sending 0 then it is a temperature value so in this case it will return null
+undefined => not defined
+symbol = to define uniqueness
+
+Not primitive data types
+object
+console.log(type of "arpit") => string
+console.log(type of null) => object
+console.log(type of undefined) => undefined because it is a type
+*/
